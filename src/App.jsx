10fd45321a1455c4cd9fe750d3842de497b8443e
Gustavo Jimenez <gusweb71@gmail.com>
2023-01-25 17:52:15 -0500

    Separamos la primera palabra.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMG_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 export const App = () => {
-  const [fact, setFact] = useState('lorem ipsum cat fact')
+  const [fact, setFact] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const { fact } = data
+        setFact(data.fact)
+        const firstWord = fact.split(' ')[0]
+        console.log(firstWord)
+      })
   }, [])
 
   return (
