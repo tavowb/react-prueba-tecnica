2cbd8b53bef9ace51cbdbf52fcad11903cf38c66
Gustavo Jimenez <gusweb71@gmail.com>
2023-01-26 16:56:09 -0500

    cargamos la img

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 APIs:
 
 - Facts Random: https://catfact.ninja/fact
-- Imagen random: https://cataas.com/cat/says/hello
-    - Enpoint para usar: `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+- Imagen random: https://api.unsplash.com/
+    - Enpoint para usar: `https://api.unsplash.com/search/photos/?query=${FirstWords}&client_id=VCGSB3pL52YWHdmVFEwuWeaCK4NR-KgSj_duTOIQ4uE`
 
-- Recupera un hecho aleatorio de gatos de la primera API
-- Recuperar la primera palabra del hecho
-- Muestra una imagen de un gato con la primera palabra.
+- Recupera un hecho aleatorio de gatos de la primera API.
+- Recuperar la primera palabra del hecho.
+- Muestra una imagen con relacion a las primeras palabras.
+- Luego de terminar todo le metemos estilos.
+- Luego refactorizar lo que se pueda.
+- Test.
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
 import { useState, useEffect } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMG_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 export const App = () => {
   const [fact, setFact] = useState()
+  const [Word, setWord] = useState()
+  const [imageUrl, setImageUrl] = useState()
+  console.log('renderizo')
 
+  const peticion = async (FirstWords) => {
+    console.log('peticion de img')
+    console.log(fact)
+    console.log(Word)
+    console.log(imageUrl)
+    const res = await fetch(`https://api.unsplash.com/search/photos/?query=${FirstWords}&client_id=VCGSB3pL52YWHdmVFEwuWeaCK4NR-KgSj_duTOIQ4uE`)
+    const dataf = await res.json()
+    setImageUrl(dataf.results[0].urls.regular)
+  }
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(data.fact)
-        const firstWord = fact.split(' ')[0]
-        console.log(firstWord)
+        setWord(fact.split(' ', 2).join(' '))
       })
   }, [])
 
+  useEffect(() => {
+    peticion(Word)
+  }, [Word])
+
   return (
     <main>
-      <h1>App de gatos</h1>
+      <h1>App de imagenes</h1>
       {fact && <p> {fact} </p>}
+      {imageUrl && <img src={imageUrl} alt='cat' />}
     </main>
   )
 }
